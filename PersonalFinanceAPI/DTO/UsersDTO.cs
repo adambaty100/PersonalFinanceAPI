87c06589e9f4063a817024e47c83ed6f2e8f7830
Adam Baty <adambaty100@gmail.com>
2026-03-14 19:20:05 -0400

    Apply gitignore correctly

--- PersonalFinanceAPI/DTO/UsersDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PersonalFinanceAPI.DTO
-{
-    public class UsersDTO
-    {
-        public string Email { get; set; } = null!;
-        public string FirstName { get; set; } = null!;
-        public string LastName { get; set; } = null!;
-    }
-}
